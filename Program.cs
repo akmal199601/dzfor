6353d038eaae9382212f9505988e4df802519650
akmal199601 <akmal199601@mail.ru>
2020-03-31 17:32:16 +0500

    Corrected

--- Program.cs
+++ Program.cs
@@ -1,103 +1,119 @@
-﻿using System.Data;
-using System.Runtime.ConstrainedExecution;
-using System;
+﻿// using System.Data;
+// using System.Runtime.ConstrainedExecution;
+// using System;
 
-namespace dz28._03
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            /// Задание 2
-            /// 2.1
-            //    int a = 2;
-            //    int b = 12;
-            //     for (int i = a; i < b; i++)
-            //     {
-            //         System.Console.WriteLine(i);
-            //     }
-            //2.2
+// namespace dz28._03
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             /// Задание 2
+//             /// 2.1
+//             // int a = 1;
+//             // int b =9 ;
+//             // int z = 0;
+//             // for (int i = a; i <= b; i++)
+//             // {
+//             //     z+=i;
+//             // System.Console.WriteLine(z);
+//             // }
 
 
-            /// Задание 3
-            // ПРЯМОУГОЛЬНИК
+//             //2.2
+//                 // int a = 2;
+//                 // int b = 12,k=0;
 
-            //  for(int i = 0;i < 5;i++){
-            //      for(int z = 0;z<22; z++){
-            //          System.Console.Write("*");
-            //      }
-            //      System.Console.WriteLine();
-            //  }
-            //  Console.ReadKey();
-            // Прямой треугольник
-            // for(int i = 0;i < 5;i++){
-            //     for(int z = 0;z <= i;z++){
-            //         System.Console.Write("* ");
-            //     }
-            //     System.Console.WriteLine();
-            // }
-            // Равностророний треугольник 
-            // int z = 0, o = 10;
-            // for (int i = o; i >= 0; i--)
-            // {
-            //     z++;
-            //     for (int x = o; x >= 0; x--)
-            //     {
-            //         Console.Write(" ");
-            //     }
-            //     o--;
-            //     for (int y = 0; y < z; y++)
-            //     {
-            //         Console.Write("* ");
-            //     }
-            //     System.Console.WriteLine();
-            //  Ромб
-            // int o = 5;
-            // int z = 0;
-            // for (int i = o; i >= 0; i--)
-            // {
-            //     z++;
-            //     for (int x = o; x >= 0; x--)
-            //     {
-            //         Console.Write(" ");
-            //     }
-            //     o--;
-            //     for (int y = 0; y < z; y++)
-            //     {
-            //         Console.Write("* ");
-            //     }
-            //     System.Console.WriteLine();
-            // }
-            // o = 5;
-            // z = 0;
-            // for (int i = 0; i <= 5; i++)
-            // {
-            //     z++;
-            //     for (int x = 0; x <= z; x++)
-            //     {
-            //         Console.Write(" ");
-            //     }
-            //     o--;
-            //     for (int y = o; y >= 0; y--)
-            //     {
-            //         Console.Write("* ");
-            //     }
-            //     System.Console.WriteLine();
-            // }
+//                 // if(a<b)
+//                 // {
+//                 //     for(int i = a;i<b;i++){
+//                 //         if(i%2==1)
+//                 //         {
+//                 //             System.Console.WriteLine(i);
+//                 //         }
+//                     }
+                   
+//                 }
 
-                //#3
-                double v = 1000, p = 5, r = 0;
-                for (double i = v; v < 1100;)
-                {
-                    r++;
-                    v += v * p / 100;
-                    System.Console.WriteLine(v);
-                }
-                System.Console.WriteLine(r);
-                Console.ReadKey();
-        }
-    }
-}
+//             /// Задание 3
+//             // ПРЯМОУГОЛЬНИК
+
+//             //  for(int i = 0;i < 5;i++){
+//             //      for(int z = 0;z<22; z++){
+//             //          System.Console.Write("*");
+//             //      }
+//             //      System.Console.WriteLine();
+//             //  }
+//             //  Console.ReadKey();
+//             // Прямой треугольник
+//             // for(int i = 0;i < 5;i++){
+//             //     for(int z = 0;z <= i;z++){
+//             //         System.Console.Write("* ");
+//             //     }
+//             //     System.Console.WriteLine();
+//             // }
+//             // Равностророний треугольник 
+//             // int z = 0, o = 10;
+//             // for (int i = o; i >= 0; i--)
+//             // {
+//             //     z++;
+//             //     for (int x = o; x >= 0; x--)
+//             //     {
+//             //         Console.Write(" ");
+//             //     }
+//             //     o--;
+//             //     for (int y = 0; y < z; y++)
+//             //     {
+//             //         Console.Write("* ");
+//             //     }
+//             //     System.Console.WriteLine();
+//             //  Ромб
+//             // int o = 5;
+//             // int z = 0;
+//             // for (int i = o; i >= 0; i--)
+//             // {
+//             //     z++;
+//             //     for (int x = o; x >= 0; x--)
+//             //     {
+//             //         Console.Write(" ");
+//             //     }
+//             //     o--;
+//             //     for (int y = 0; y < z; y++)
+//             //     {
+//             //         Console.Write("* ");
+//             //     }
+//             //     System.Console.WriteLine();
+//             // }
+//             // o = 5;
+//             // z = 0;
+//             // for (int i = 0; i <= 5; i++)
+//             // {
+//             //     z++;
+//             //     for (int x = 0; x <= z; x++)
+//             //     {
+//             //         Console.Write(" ");
+//             //     }
+//             //     o--;
+//             //     for (int y = o; y >= 0; y--)
+//             //     {
+//             //         Console.Write("* ");
+//             //     }
+//             //     System.Console.WriteLine();
+//             // }
+
+//             //#3
+//         //     double v = 1000, p = 5, r = 0;
+//         //     for (double i = v; v < 1100;)
+//         //     {
+//         //         r++;
+//         //         v += v * p / 100;
+//         //     }
+//         //         System.Console.WriteLine(v);
+//         //     System.Console.WriteLine(r);
+//             Console.ReadKey();
+//         }
+//     }
+// }
 
 
 
